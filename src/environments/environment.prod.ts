af961e9d01a774e0949dee02590d0c7a3dfb0bef
Cristhian <70170324+CRISTHIAN390@users.noreply.github.com>
2024-11-04 00:49:50 -0500

    ChatGemini-List

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:4500',
-  GEMINI_API_KEY:'AIzaSyDmdpXS5gtk8G-MNIEPya6sfjnv9Icwkuo',
+  geminiApiKey: 'AIzaSyCefTA1BsNhDo1JvMZmVKWetRmO9lWFEZk', // Clave de API de Gemini
+  geminiBaseUrl: 'https://generativelanguage.googleapis.com/v1beta/', // URL base de la API de Gemini
+  geminiModelEndpoint: 'models/gemini-1.5-flash-latest:generateContent', // Endpoint específico
+  geminiTimeout: 30000 // Tiempo de espera en milisegundos
 };
